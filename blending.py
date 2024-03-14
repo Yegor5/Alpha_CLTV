53c53968257115c133fd588b95135c78b7f1a2e9
Yegor <92928760+Yegor5@users.noreply.github.com>
2024-03-14 06:34:47 +0300

    Update blending.py

--- blending.py
+++ blending.py
@@ -4,5 +4,5 @@ catboost = pd.read_csv('best_ctb.csv')
 neuro = pd.read_csv('preds14.csv')
 columns = list(catboost.columns)
 columns.remove('id')
-lgb[columns] = 0.5 * catboost[columns] + 0.45 * neuro[columns] + 0.1 * lgb[columns]
+lgb[columns] = 0.45 * catboost[columns] + 0.45 * neuro[columns] + 0.1 * lgb[columns]
 lgb.to_csv('all_blend.csv', index=False)
